ba78e36d7aebd922d9e88c404bf57f36a3717e02
md-hasan-ali <hasanaliringku@gmail.com>
2021-10-29 23:47:57 +0600

    added firebase initialize

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA0RAJFMfuNnLseDXzwaagj2gHaBA542Cc",
+    authDomain: "react-firebase-authentic-61f82.firebaseapp.com",
+    projectId: "react-firebase-authentic-61f82",
+    storageBucket: "react-firebase-authentic-61f82.appspot.com",
+    messagingSenderId: "200906403618",
+    appId: "1:200906403618:web:ae0a76b024794379a23f7b"
+};
+export default firebaseConfig
